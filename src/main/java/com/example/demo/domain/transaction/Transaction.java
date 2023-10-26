438faf028342de8d230b749c64d611328e9f9aee
Joaopedromach <estranhonerd123@gmail.com>
2023-10-26 13:28:11 -0300

    domain transactions

---
+++ src/main/java/com/example/demo/domain/transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain.transaction;
+import com.example.demo.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToMany
+    @JoinColumn(name = "payer_id")
+    private User payer;
+
+    @ManyToMany
+    @JoinColumn(name = "payee_id")
+    private User payee;
+
+    private LocalDateTime transactiontype;
+
+
+}
