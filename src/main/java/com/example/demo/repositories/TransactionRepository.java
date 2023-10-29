d56f57e98693a931087c3a4be2b3b3995e346e51
Joaopedromach <estranhonerd123@gmail.com>
2023-10-29 16:45:34 -0300

    repository

---
+++ src/main/java/com/example/demo/repositories/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
